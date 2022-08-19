cab6690842732cda34d0e549b9a953ea32abc014
palazzid88 <palazzid88@gmail.com>
2022-08-19 02:39:35 -0300

    TP2 sintaxis-variables-palabras-reservadas

---
+++ TP2/index.js
@@ -0,0 +1,12 @@
+const array = [
+    "David", 
+    34, 
+    true,
+    new Date (1988, 05, 06),
+    {
+        marca: "Renault",
+        nombre: "Logan",
+        modelo: "2017",
+        url: "https://www.renault.com.ar/automoviles/logan.html" ,
+    },
+]
