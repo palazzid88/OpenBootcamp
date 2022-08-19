587d118fdc5968f2d2fd0af0989906347d8561fe
palazzid88 <palazzid88@gmail.com>
2022-08-19 06:19:29 -0300

    TP3 estructuras de control

---
+++ TP3/index.js
@@ -0,0 +1,26 @@
+
+// Calculo de factorial de 10 con un bucle for
+
+// 10!
+
+let numero = 10;
+
+for (i = 1; i<11; i++) {
+    numero*=i
+    console.log(numero)
+}
+console.log("Fin");
+
+
+
+// calculo de factorial de 10 con un bucle 
+
+let numero2 = 10;
+let factorial = 1;
+
+while (factorial < 10) {
+    numero2= numero2 * factorial;
+    console.log(numero2)
+    factorial++
+    console.log(factorial)
+}
