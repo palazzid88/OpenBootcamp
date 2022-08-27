6d877c4eee9b655a28657f78c5e8c169dada75da
palazzid88 <palazzid88@gmail.com>
2022-08-27 04:30:47 -0300

    TP5 Numbers

---
+++ TP5/index.js
@@ -0,0 +1,15 @@
+
+const alturacm = 183;
+console.log(alturacm);
+
+const alturam= 1.83;
+console.log(alturam);
+
+const peso = 75.5;
+console.log(peso);
+
+const alturaRedondeada = Math.ceil(alturam);
+console.log(alturaRedondeada);
+
+const pesoRedondeado = Math.floor(peso);
+console.log(pesoRedondeado);
